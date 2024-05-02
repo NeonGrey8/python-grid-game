c66802839760fb04f7919edd9a24346617e8e838
Anders <89496587+peashooterpear63@users.noreply.github.com>
2024-05-02 15:53:55 -0400

    Added movement to the y-axis

--- main.py
+++ main.py
@@ -9,13 +9,12 @@ fifthgridarray  = [0, 0, 0, 0, 0, 0]
 sixthgridarray  = [0, 0, 0, 0, 0, 0]
 
 class Player:
-    
-    lives = 3
 
-    def __init__(self, x, y, name):
+    def __init__(self, x, y, name, lives):
         self.x = x
-        self.z = y
+        self.y = y
         self.name = name
+        self.lives = lives
     
     def movex(self, posx, grid_array):
         self.x.append(posx)
@@ -29,7 +28,7 @@ class Player:
     def die(self):
         print(self.name + " has died!")
 
-def insertpos(player_position_x, prev_position, grid_array):
+def insertpos(player_position_x, prev_pos, grid_array):
     grid_array.pop(player_position_x - 1)
     grid_array.insert(player_position_x - 1, 1)
     grid_array.pop(prev_pos - 1)
@@ -37,38 +36,115 @@ def insertpos(player_position_x, prev_position, grid_array):
 
     return grid_array
 
-def key_handler(posx):
-    key = keyboard.read_key()
-    if key == 'd':
-        posx = posx + 1
-    if key == 'a':
-        posx = posx - 1
+def insertpos2(posx, prev_pos, grid_array):
+    grid_array.pop(posx - 1)
+    grid_array.insert(posx - 1, 1)
 
-Jake = Player(3, 1, 'Jake')
+    return grid_array
+
+def deletepos(posx, grid_array):
+    grid_array.pop(posx - 1)
+    grid_array.insert(posx - 1, 0)
+
+    return grid_array
+
+Jake = Player(3, 1, 'Jake', 3)
+
 begin = input("Begin? Y/N")
 if (begin == 'Y'):
     GameRunning = True
-
-Jake.spawn(firstgridarray)
+    Jake.spawn(firstgridarray)
+    print("{}\n{}\n{}\n".format(
+        firstgridarray, secondgridarray, thirdgridarray
+    ))
 
 while GameRunning == True:
     prev_pos = Jake.x
-
+    
+    if (Jake.lives < 1):
+        Jake.die()
+    
     key = keyboard.read_key()
     if key == 'd':
         Jake.x = Jake.x + 1
-        firstgridarray = insertpos(Jake.x, prev_pos, firstgridarray)
+        if Jake.y == 1:
+            firstgridarray = insertpos(Jake.x, prev_pos, firstgridarray)
+        if Jake.y == 2:
+            secondgridarray = insertpos(Jake.x, prev_pos, secondgridarray)
+        if Jake.y == 3:
+            thirdgridarray = insertpos(Jake.x, prev_pos, thirdgridarray)
+        if Jake.y == 4:
+            fourthgridarray = insertpos(Jake.x, prev_pos, fourthgridarray)
+        if Jake.y == 5:
+            fifthgridarray = insertpos(Jake.x, prev_pos, fifthgridarray)
+    
     if key == 'a':
         Jake.x = Jake.x - 1
-        firstgridarray = insertpos(Jake.x, prev_pos, firstgridarray)
-    #if key == 's':
-    #    Jake.y = Jake.y + 1
+        if Jake.y == 1:
+            firstgridarray = insertpos(Jake.x, prev_pos, firstgridarray)
+        if Jake.y == 2:
+            secondgridarray = insertpos(Jake.x, prev_pos, secondgridarray)
+        if Jake.y == 3:
+            thirdgridarray = insertpos(Jake.x, prev_pos, thirdgridarray)
+        if Jake.y == 4:
+            fourthgridarray = insertpos(Jake.x, prev_pos, fourthgridarray)
+        if Jake.y == 5:
+            fifthgridarray = insertpos(Jake.x, prev_pos, fifthgridarray)
+
+    if key == 's':
+
+        if Jake.y == 1:
+            firstgridarray = deletepos(Jake.x, firstgridarray)
+        if Jake.y == 2:
+            secondgridarray = deletepos(Jake.x, secondgridarray)
+        if Jake.y == 3:
+            thirdgridarray = deletepos(Jake.x, thirdgridarray)
+        if Jake.y == 4:
+            fourthgridarray = deletepos(Jake.x, fourthgridarray)
+        if Jake.y == 5:
+            fifthgridarray = deletepos(Jake.x, fifthgridarray)
+
+        Jake.y = Jake.y + 1
+
+        if Jake.y == 1:
+            firstgridarray = insertpos2(Jake.x, prev_pos, firstgridarray)
+        if Jake.y == 2:
+            secondgridarray = insertpos2(Jake.x, prev_pos, secondgridarray)
+        if Jake.y == 3:
+            thirdgridarray = insertpos2(Jake.x, prev_pos, thirdgridarray)
+        if Jake.y == 4:
+            fourthgridarray = insertpos2(Jake.x, prev_pos, fourthgridarray)
+        if Jake.y == 5:
+            fifthgridarray = insertpos2(Jake.x, prev_pos, fifthgridarray)
     
-    #if (Jake.y == 1):
-    #    firstgridarray
-    print("{}\n{}\n{}\n{}\n{}\n{}\n".format(
-        firstgridarray, secondgridarray, thirdgridarray,
-        fourthgridarray, fifthgridarray, sixthgridarray
-        ))
+    if key == 'w':
+
+        if Jake.y == 1:
+            firstgridarray = deletepos(Jake.x, firstgridarray)
+        if Jake.y == 2:
+            secondgridarray = deletepos(Jake.x, secondgridarray)
+        if Jake.y == 3:
+            thirdgridarray = deletepos(Jake.x, thirdgridarray)
+        if Jake.y == 4:
+            fourthgridarray = deletepos(Jake.x, fourthgridarray)
+        if Jake.y == 5:
+            fifthgridarray = deletepos(Jake.x, fifthgridarray)
+
+        Jake.y = Jake.y - 1
+
+        if Jake.y == 1:
+            firstgridarray = insertpos2(Jake.x, prev_pos, firstgridarray)
+        if Jake.y == 2:
+            secondgridarray = insertpos2(Jake.x, prev_pos, secondgridarray)
+        if Jake.y == 3:
+            thirdgridarray = insertpos2(Jake.x, prev_pos, thirdgridarray)
+        if Jake.y == 4:
+            fourthgridarray = insertpos2(Jake.x, prev_pos, fourthgridarray)
+        if Jake.y == 5:
+            fifthgridarray = insertpos2(Jake.x, prev_pos, fifthgridarray)
+    
+    print("{}\n{}\n{}\n{}\n{}".format(
+        firstgridarray, secondgridarray, thirdgridarray, fourthgridarray, fifthgridarray
+    ))
+    print(Jake.y)
     time.sleep(0.5)
-print(Jake.x)
